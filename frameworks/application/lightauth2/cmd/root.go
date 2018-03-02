50bdfa11d742c9d962243779f4bcdabbe29d8916
Unknown <gary@garyleeson.com>
2018-03-02 21:19:18 +0000

    Auto stash before rebase of "master"

---
+++ frameworks/application/lightauth2/cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "lightauth2",
+	Short: "lightauth2 is an authentication server and session key generator",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
